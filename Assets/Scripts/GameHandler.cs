ce07aac9478a1fcf51c6883a80072d76d1b75c8f
MLBOT86 <mlbot86@mail.ru>
2024-04-30 06:35:40 +0300

    first commit
    start

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,44 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey;
+using CodeMonkey.Utils;
+
+public class GameHandler : MonoBehaviour {
+
+    [SerializeField] private Snake snake;
+
+    private LevelGreed levelGreed;
+    private void Start() {
+        Debug.Log("GameHandler.Start");
+        levelGreed = new LevelGreed(20, 20);
+        snake.Setup(levelGreed);
+        levelGreed.Setup(snake);
+
+        //int number = 0;
+
+       // FunctionPeriodic.Create(() =>
+       // {
+           // CMDebug.TextPopupMouse("Ding!" + number);
+          //  number++;
+      //  },.3f);
+
+
+       // GameObject snakeHeadGameObject = new GameObject();
+       // SpriteRenderer snakeSpriteRenderer = snakeHeadGameObject.AddComponent<SpriteRenderer>();
+       // snakeSpriteRenderer.sprite = GameAssets.i.snakeHeadSprite;
+    }
+
+}
