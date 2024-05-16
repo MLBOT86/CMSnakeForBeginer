5b0aa734c484a66fdda0267d01fcb27486336509
MLBOT86 <mlbot86@mail.ru>
2024-05-16 12:05:18 +0300

    160524
    9 lesson

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -32,12 +32,18 @@ public class GameHandler : MonoBehaviour {
     private void Awake()
     {
         instance = this;
+        InitializeStatic();
     }
     private void Start() {
        // Debug.Log("GameHandler.Start");
         levelGreed = new LevelGreed(20, 20);
         snake.Setup(levelGreed);
         levelGreed.Setup(snake);
+
+        CMDebug.ButtonUI(Vector2.zero, "Reload Scene", () =>
+        {
+            Loader.Load(Loader.Scene.SampleScene);
+        });
 
         //int number = 0;
 
@@ -51,6 +57,10 @@ public class GameHandler : MonoBehaviour {
        // GameObject snakeHeadGameObject = new GameObject();
        // SpriteRenderer snakeSpriteRenderer = snakeHeadGameObject.AddComponent<SpriteRenderer>();
        // snakeSpriteRenderer.sprite = GameAssets.i.snakeHeadSprite;
+    }
+    private static void InitializeStatic()
+    {
+        score = 0;
     }
     public static int GetScore()
     {
