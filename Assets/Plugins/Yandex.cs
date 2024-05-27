ae03785e80ca2c108ea1d970a5a9972bc60b86d5
MLBOT86 <mlbot86@mail.ru>
2024-05-27 23:12:38 +0300

    270524
    it was long long day.
    giveMePlayerData metod
    RateGame metod

--- Assets/Plugins/Yandex.cs
+++ Assets/Plugins/Yandex.cs
@@ -1,17 +1,58 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Drawing;
+
 using System.Runtime.InteropServices;
-using UnityEditor.PackageManager.UI;
+using TMPro;
+
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class Yandex : MonoBehaviour
 {
+
+
     [DllImport("__Internal")]
     private static extern void RateGame();
 
 
+    [DllImport("__Internal")]
+    private static extern void GiveMePlayerData();
+
+    [SerializeField] TextMeshProUGUI _nameText;
+    [SerializeField] RawImage _photo;
+
+
+    public void HelloButton()
+    {
+        GiveMePlayerData();
+    }
+
+
+    public void SetName(string name)
+    {
+        _nameText.text = name;
+    }
+
+    public void SetPhoto(string url)
+    {
+        StartCoroutine(DownLoadImage( url));
+    }
+
+    IEnumerator DownLoadImage(string mediaUrl)
+    {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(mediaUrl);
+        yield return request.SendWebRequest();
+        if(request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+        
+            Debug.Log(request.error);
+        
+        else
+        
+            _photo.texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+        
+
+    }
 
     public void RateGameBtn()
     {
